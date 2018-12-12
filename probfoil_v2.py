1635054f7aacb2fbf834e36d5076ff0cfb2df544
Kehkashan Fazal <fazal.kehkashan@gmail.com>
2018-12-11 22:25:13 -0800

    added pi

--- probfoil_v2.py
+++ probfoil_v2.py
@@ -9,12 +9,13 @@ import argparse
 import sys
 
 class Predicate:
-    def __init__(self, name, arity, pi_dict, types):
+    def __init__(self, name, arity, groundings, pi_dict, types):
         self.name = name
         self.arity = arity
         self.pi_dict = pi_dict #keys are groundings and values are pi for them
-#       self.groundings = groundings
+        self.groundings = groundings
         self.types = types
+        
 
 class DataFile(object):
     #file where all querying and db extension happens
@@ -82,30 +83,38 @@ def main(argv=sys.argv[1:]):
                              % (typeinfo.functor, len(argtypes)))
         else:
             types[key] = argtypes
-        print(key)
+    print(types)
 
     for modeinfo in data.query('mode', 1):
         modeinfo = modeinfo[0]
         modes.append((modeinfo.functor, list(map(str, modeinfo.args))))
-        print(modes)
-    print(types.items())
-    for predicate, types in types.items():
+    #     print(modes)
+    # print(types.items())
+    for predicate, type_el in types.items():
         arg_values = zip(*data.query(*predicate))
-        for a, t in zip(arg_values, types):
+        for a, t in zip(arg_values, type_el):
             for value in a:
                 values[predicate].add(value)
                 groundings[t].add(value) 
-    print(values)
-    print(groundings) #predicate name, arity is the key and value: actual groundings
+    # print(values)
+    # print(groundings) #predicate name, arity is the key and value: actual groundings
     # grounding is also a dicyionary
-    for i in types.keys():
+    # print(values)
+    # print("Types:", types)
+    Literals_List = []
+    for i in values.keys():
         name = i[0]
         arity = i[1]
-        g = values[i]
-        pidata.evaluate(None, i[0], g)
+        g = tuple(values[i])
+        print(g)
+        # print(name, arity, g)
+        g1 = [r for r in data.query(name, arity)]
+        print(g1)
+        temp = Predicate(name, arity, g1, data.evaluate(None, name, g1, None), types[i])
+        Literals_List.append(temp)
+    print(Literals_List)
+        
 
-        temp_object = new Predicate()
-    Literals_List = []
     
     
     
@@ -118,58 +127,58 @@ if __name__ == '__main__':
     main()
 
 
-def rule_Calc(rule):
-    trial_pi_phi= data.evaluate(rule, arguments=groundings)
-    t_pos = 0.0 
-    f_pos = 0.0  
-    t_neg = 0.0
-    f_neg = 0.0
-    pi_list= prev_list
-    phi_list= list(trial_pi_phi.values())
-    # need to re-confirm how to do this.is evaluate, ground and values used here
-    for pi, phi in zip(pi_list,phi_list): # need to confirm what to put here.wonky answers!
-      ni=1- pi
-      nhi=1-phi
-      t_pos_i= min(pi,phi)
-      t_neg_i= min(ni, nhi)
-      f_pos_i= max(0, t_neg_i - ni)
-      f_neg_i=  max(0,pi- t_pos_i)
-      t_pos+=t_pos_i 
-      f_pos+= f_pos_i
-      f_neg+= f_neg_i
-      t_neg+= t_neg_i
-    return t_pos, f_pos, t_neg, f_neg
+# def rule_Calc(rule):
+#     trial_pi_phi= data.evaluate(rule, arguments=groundings)
+#     t_pos = 0.0 
+#     f_pos = 0.0  
+#     t_neg = 0.0
+#     f_neg = 0.0
+#     pi_list= prev_list
+#     phi_list= list(trial_pi_phi.values())
+#     # need to re-confirm how to do this.is evaluate, ground and values used here
+#     for pi, phi in zip(pi_list,phi_list): # need to confirm what to put here.wonky answers!
+#       ni=1- pi
+#       nhi=1-phi
+#       t_pos_i= min(pi,phi)
+#       t_neg_i= min(ni, nhi)
+#       f_pos_i= max(0, t_neg_i - ni)
+#       f_neg_i=  max(0,pi- t_pos_i)
+#       t_pos+=t_pos_i 
+#       f_pos+= f_pos_i
+#       f_neg+= f_neg_i
+#       t_neg+= t_neg_i
+#     return t_pos, f_pos, t_neg, f_neg
 
-def finding_accuracy(rule):
-    t_pos, f_pos, t_neg, f_neg= rule_Calc(rule)
-    return (t_pos + t_neg)/ (t_pos + f_pos + t_neg + f_neg)
-def finding_recall(rule):
-    t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
-    return (t_pos) / (t_pos + f_neg)
-def finding_precision(rule):
-    t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
-    return (t_pos) / (t_pos + f_pos)
-def finding_m_est(rule):
-    t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
-    pos= t_pos + f_neg
-    neg= t_neg+f_pos
-    return (t_pos+(m* pos/(pos+neg)))/ (t_pos+f_pos+m)
+# def finding_accuracy(rule):
+#     t_pos, f_pos, t_neg, f_neg= rule_Calc(rule)
+#     return (t_pos + t_neg)/ (t_pos + f_pos + t_neg + f_neg)
+# def finding_recall(rule):
+#     t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
+#     return (t_pos) / (t_pos + f_neg)
+# def finding_precision(rule):
+#     t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
+#     return (t_pos) / (t_pos + f_pos)
+# def finding_m_est(rule):
+#     t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
+#     pos= t_pos + f_neg
+#     neg= t_neg+f_pos
+#     return (t_pos+(m* pos/(pos+neg)))/ (t_pos+f_pos+m)
   
-def local_score(H, c):
-    list_h_u_c= H + c
-    l_score= finding_m_est(list_h_u_c)- finding_m_est(H)
-    return l_score
-def local_stop(H,c):
-    list_h_u_c= H + c
-    t_pos_h_c, f_pos_h_c, t_neg_h_c, f_neg_h_c= rule_Calc(list_h_u_c)
-    t_pos_c, f_pos_c, t_neg_c, f_neg_c= rule_Calc(c)
-    t_pos_h, f_pos_h, t_neg_h, f_neg_h= rule_Calc(H)
-    a= t_pos_h_c- t_pos_h
-    b= f_pos_c
-    print ("a=", a, "b=",b)
-    if(a==0 or b=0):
-      return 1
-    else
-      return 0
+# def local_score(H, c):
+#     list_h_u_c= H + c
+#     l_score= finding_m_est(list_h_u_c)- finding_m_est(H)
+#     return l_score
+# def local_stop(H,c):
+#     list_h_u_c= H + c
+#     t_pos_h_c, f_pos_h_c, t_neg_h_c, f_neg_h_c= rule_Calc(list_h_u_c)
+#     t_pos_c, f_pos_c, t_neg_c, f_neg_c= rule_Calc(c)
+#     t_pos_h, f_pos_h, t_neg_h, f_neg_h= rule_Calc(H)
+#     a= t_pos_h_c- t_pos_h
+#     b= f_pos_c
+#     print ("a=", a, "b=",b)
+#     if(a==0 or b=0):
+#       return 1
+#     else
+#       return 0
   
  
