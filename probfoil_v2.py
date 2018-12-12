db576757893b68c04584356205df3a9e53132c73
path <whizkidrohan@gmail.com>
2018-12-12 00:16:01 -0800

    asdf

--- probfoil_v2.py
+++ probfoil_v2.py
@@ -65,6 +65,64 @@ class DataFile(object):
         #output looks something like this:
         #{flies(1): 1.0, flies(2): 1.0, flies(3): 1.0, flies(4): 1.0, flies(5): 1.0, flies(6): 1.0, flies(7): 1.0, flies(8): 1.0, flies(9): 1.0, flies(10): 0.0}
 
+def argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', nargs='+')
+    return parser
+
+def rule_Calc(rule):
+    t_pos = 0.0 
+    f_pos = 0.0  
+    t_neg = 0.0
+    f_neg = 0.0
+    pi_list= prev_list
+    phi_list= list(trial_pi_phi.values())
+    # need to re-confirm how to do this.is evaluate, ground and values used here
+    for pi, phi in zip(pi_list,phi_list): # need to confirm what to put here.wonky answers!
+      ni=1- pi
+      nhi=1-phi
+      t_pos_i= min(pi,phi)
+      t_neg_i= min(ni, nhi)
+      f_pos_i= max(0, t_neg_i - ni)
+      f_neg_i=  max(0,pi- t_pos_i)
+      t_pos+=t_pos_i 
+      f_pos+= f_pos_i
+      f_neg+= f_neg_i
+      t_neg+= t_neg_i
+    return t_pos, f_pos, t_neg, f_neg
+
+def finding_accuracy(rule):
+    t_pos, f_pos, t_neg, f_neg= rule_Calc(rule)
+    return (t_pos + t_neg)/ (t_pos + f_pos + t_neg + f_neg)
+def finding_recall(rule):
+    t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
+    return (t_pos) / (t_pos + f_neg)
+def finding_precision(rule):
+    t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
+    return (t_pos) / (t_pos + f_pos)
+def finding_m_est(rule):
+    t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
+    pos= t_pos + f_neg
+    neg= t_neg+f_pos
+    return (t_pos+(m* pos/(pos+neg)))/ (t_pos+f_pos+m)
+  
+def local_score(H, c):
+    list_h_u_c= H + c
+    l_score= finding_m_est(list_h_u_c)- finding_m_est(H)
+    return l_score
+def local_stop(H,c):
+    list_h_u_c= H + c
+    t_pos_h_c, f_pos_h_c, t_neg_h_c, f_neg_h_c= rule_Calc(list_h_u_c)
+    t_pos_c, f_pos_c, t_neg_c, f_neg_c= rule_Calc(c)
+    t_pos_h, f_pos_h, t_neg_h, f_neg_h= rule_Calc(H)
+    a= t_pos_h_c- t_pos_h
+    b= f_pos_c
+    print ("a=", a, "b=",b)
+    if(a==0 or b==0):
+      return 1
+    else
+      return 0
+
 
 
 
@@ -124,98 +182,34 @@ def main(argv=sys.argv[1:]):
         if i.name == target_name:
             prev_list = i.pi_dict.values()
 
-def argparser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('files', nargs='+')
-    return parser
+    #anchal code
+    #target_predicate 
+    h = [] #list of name and arity, can generalize more later[p1,p2]
+    H = []
+    b = []
+    c = [] #dont think we need this
 
-
-#anchal code
-#target_predicate 
-h = [] #list of name and arity, can generalize more later[p1,p2]
-H = []
-b = []
-c = [] #dont think we need this
-
-while True:
-  b = []
-  ls = localstop(H,b)
-  while not ls:
-    arg_max = -10000 # change to 0?
-    l_2b_added = 0
-    for l in literals:
-      score = localscore(b,l) #b is a list of element same as l
-      if score > arg_max:
-        arg_max = score
-        l_2b_added = l
-    if l == -10000:
-      print "*******Error: no literal is being added, local score not working"
-    b = b + l
-    ls = localstop(H,b)
-  #extra pruning neglecting
-  if globalscore(H) < globalscore(H + b):
-    H = H + b
-  if (globalscore(H) > globalscore(H + b)):
-    break
-return H
-
-
-
+    while True:
+      b = []
+      ls = localstop(H,b)
+      while not ls:
+        arg_max = -10000 # change to 0?
+        l_2b_added = 0
+        for l in literals:
+          score = localscore(b,l) #b is a list of element same as l
+          if score > arg_max:
+            arg_max = score
+            l_2b_added = l
+        if l == -10000:
+          print "*******Error: no literal is being added, local score not working"
+        b = b + l
+        ls = localstop(H,b)
+      #extra pruning neglecting
+      if globalscore(H) < globalscore(H + b):
+        H = H + b
+      if (globalscore(H) > globalscore(H + b)):
+        break
+    return H
 
 if __name__ == '__main__':
     main()
-
-
-def rule_Calc(rule):
-    t_pos = 0.0 
-    f_pos = 0.0  
-    t_neg = 0.0
-    f_neg = 0.0
-    pi_list= prev_list
-    phi_list= list(trial_pi_phi.values())
-    # need to re-confirm how to do this.is evaluate, ground and values used here
-    for pi, phi in zip(pi_list,phi_list): # need to confirm what to put here.wonky answers!
-      ni=1- pi
-      nhi=1-phi
-      t_pos_i= min(pi,phi)
-      t_neg_i= min(ni, nhi)
-      f_pos_i= max(0, t_neg_i - ni)
-      f_neg_i=  max(0,pi- t_pos_i)
-      t_pos+=t_pos_i 
-      f_pos+= f_pos_i
-      f_neg+= f_neg_i
-      t_neg+= t_neg_i
-    return t_pos, f_pos, t_neg, f_neg
-
-def finding_accuracy(rule):
-    t_pos, f_pos, t_neg, f_neg= rule_Calc(rule)
-    return (t_pos + t_neg)/ (t_pos + f_pos + t_neg + f_neg)
-def finding_recall(rule):
-    t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
-    return (t_pos) / (t_pos + f_neg)
-def finding_precision(rule):
-    t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
-    return (t_pos) / (t_pos + f_pos)
-def finding_m_est(rule):
-    t_pos, f_pos, t_neg, f_neg = rule_Calc(rule)
-    pos= t_pos + f_neg
-    neg= t_neg+f_pos
-    return (t_pos+(m* pos/(pos+neg)))/ (t_pos+f_pos+m)
-  
-def local_score(H, c):
-    list_h_u_c= H + c
-    l_score= finding_m_est(list_h_u_c)- finding_m_est(H)
-    return l_score
-def local_stop(H,c):
-    list_h_u_c= H + c
-    t_pos_h_c, f_pos_h_c, t_neg_h_c, f_neg_h_c= rule_Calc(list_h_u_c)
-    t_pos_c, f_pos_c, t_neg_c, f_neg_c= rule_Calc(c)
-    t_pos_h, f_pos_h, t_neg_h, f_neg_h= rule_Calc(H)
-    a= t_pos_h_c- t_pos_h
-    b= f_pos_c
-    print ("a=", a, "b=",b)
-    if(a==0 or b==0):
-      return 1
-    else
-      return 0
-
